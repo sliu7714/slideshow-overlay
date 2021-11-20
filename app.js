d79a66aa2633b5d7d7c0bdd321894217548fa922
sliu7714 <siyuanliu3@gmail.com>
2021-11-19 22:29:47 -0500

    added static directory middleware for public folder

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 // for heroku deployment
+
+const express = require('express')
+const app = express();
+
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, '/public')))
+
+// app.use(express.static('public'))
+
+
+app.get('/', (req, res) => {
+    res.send('<a href="static/public/examples/examples.html">click here to go to the example html page</a>')
+})
+
+const port = process.env.PORT || 5000
+app.listen(port, () => {
+    console.log(`listening on port: ${port}`)
+})
